0d156861274c31d03df85c18c91ad0b556deab76
mknyambura <kariuki.mercie@gmail.com>
2023-02-08 09:25:04 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
+    def name=(name)
+      @name = name
+    end
+    
+      # getter method
+    def name
+      @name
+    end
 
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 end
+
+mastiff = Dog.new
+
+mastiff.name=("Mastiff")
+mastiff.name
+
+mastiff.breed=("Chihuahua")
+mastiff.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
+    def name=(name)
+      @name = name
+    end
+    
+      # getter method
+    def name
+        @name
+    end
 
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
 end
+
+kanye = Person.new
+kanye.name=("Kanye")
+kanye.job=("Cloud Security Engineer")
+
+kanye.name
+kanye.job
